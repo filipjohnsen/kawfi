5ecd8b149005492adab263771d210fcbdc928dd9
Filip Johnsen <filipjohnsen97@gmail.com>
2023-06-11 21:35:49 +0200

    updated header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export const Header = () => {
   return (
     <header>
-      <nav className="h-12 flex items-center justify-between max-w-[900px] sticky top-2 mx-auto rounded-full shadow-md px-4">
+      <nav className="h-12 flex items-center justify-between max-w-[900px] sticky top-2 mx-auto rounded-full shadow-md px-8">
         <Link className="text-sm uppercase font-bold" href="/">
           <i>Kawfi ☕️</i>
         </Link>
